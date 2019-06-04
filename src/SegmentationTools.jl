9cec91e773f922fec3982b0731bff0f7215b2c37
Tamas <tamas@tamasnagy.com>
2019-06-04 14:43:26 -0700

    add ability to do seeded compact watershed over timeseries

--- src/SegmentationTools.jl
+++ src/SegmentationTools.jl
@@ -1,8 +1,14 @@
 module SegmentationTools
 
 using DataFrames
+using AxisArrays
+using Images
+using ImageAxes
+using ImageSegmentation
+using ImageMorphology
 using Colors
 
+include("segment.jl")
 include("diagnostics.jl")
 
 end # module
---
+++ src/segment.jl
@@ -0,0 +1,41 @@
+"""
+    segment_cells(img, seed_channel, segment_channel, maskfunc)
+"""
+function segment_cells(img::AxisArray{T, 4}, 
+                       seed_channel::Symbol, 
+                       segment_channel::Symbol,
+                       maskfunc::Function) where {T}
+    if !all(map(x->x in axisnames(img), [:time, :channel]))
+        error("image requires :channel and :time axes")
+    end
+    tax = timeaxis(img)
+    segments = Array{SegmentedImage{Array{Int64,2},Float32}}(undef, length(tax))
+    ylen, xlen = size(img, Axis{:y}), size(img, Axis{:x})
+    markers = zeros(Int, ylen, xlen)
+    mask = Array{Bool}(undef, ylen, xlen)
+    segslice = Array{T}(undef, ylen, xlen)
+    seedslice = Array{T}(undef, ylen, xlen)
+    seeds = Array{Bool}(undef, ylen, xlen)
+
+    for (idx, tp) in enumerate(tax.val)
+        
+        # find seeds
+        seedslice .= view(img, Axis{:channel}(seed_channel), Axis{:time}(tp))
+        seeds .= seedslice .> otsu_threshold(seedslice)*1.1
+        
+        # find segmentation slice
+        segslice .= view(img, Axis{:channel}(segment_channel), Axis{:time}(tp))
+        
+        # throw out seeds that are outside of segment areas
+        seeds .&= (segslice .> 0.0)
+        ImageMorphology.closing!(seeds)
+        ImageMorphology.label_components!(markers, seeds, 1:ndims(seeds));
+        
+        # compute mask
+        mask .= maskfunc(segslice)
+        ImageMorphology.closing!(mask)
+        
+        segments[idx] = watershed(1 .- imadjustintensity(segslice), markers, compactness=0.01, mask=mask)
+    end
+    segments
+end
