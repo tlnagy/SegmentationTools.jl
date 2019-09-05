d1d9cecc47ab05a89ecb74475217f7b5fcf667c7
Tamas Nagy <tamas@tamasnagy.com>
2019-09-05 13:13:37 -0700

    add more testing of local background subtraction

--- test/runtests.jl
+++ test/runtests.jl
@@ -54,30 +54,42 @@ end
     img = zeros(Gray, 100, 100, 5)
 
     # create three cells
-    centers = [Point(20, 20), Point(25, 17), Point(70, 70), Point(16, 80)]
-    radii = [5, 5, 5, 7]
+    centers = [Point(20, 20), Point(25, 17), Point(70, 70), Point(70, 70), Point(16, 80)]
+    radii = [5, 5, 11, 5, 7]
 
-    # draw the "cells" and make them grow dimmer over time
+    bkg = 0.1
+
     for i in 1:size(img, 3)
-        ImageDraw.draw!(view(img, :, :, i), [ImageDraw.CirclePointRadius(c, r) for (c,r) in zip(centers, radii)], Gray(1.0/i))
+        # draw the "cells" and make them grow dimmer over time
+        colors = [Gray(1.0/i) for _ in centers]
+        # the 3rd circle is concentric with the 3rd and is a high background
+        colors[3] = Gray(bkg)
+        ImageDraw.draw!(view(img, :, :, i), [ImageDraw.CirclePointRadius(c, r) for (c,r) in zip(centers, radii)], colors)
     end
 
     wrapped = AxisArray(img, Axis{:y}(1μm:1μm:100μm), Axis{:x}(1μm:1μm:100μm), Axis{:time}(1:5));
     
-    thresholds = AxisArray(img .> 0.0, AxisArrays.axes(wrapped))
-    particles = SegmentationTools.build_tp_df(wrapped, thresholds)
+    mask = AxisArray(img .> bkg, AxisArrays.axes(wrapped))
+    particles = SegmentationTools.build_tp_df(wrapped, mask)
 
+    # three timepoints and three separate particles
+    @test size(particles, 1) == 3*5 
     # verify that the total fluorescence values in the dataframe grow dimmer as
     # we expect
     @test all(particles[particles[!, :id] .== 1, :tf_slice] ./ 145.0 .≈ [1.0/i for i in 1:5])
 
+    # for the third particle verify that the total fluorescence values have the
+    # correct amount of background subtracted off, i.e for a background value of
+    # `bkg` the final value should be tf - area * bkg
+    @test all(particles[particles[!, :id] .== 3, :tf_slice] .≈ [69/i - 69*bkg for i in 1:5])
+
     # test centroids
-    @test particles[1, :x] == centers[4].x
-    @test particles[1, :y] == centers[4].y
+    @test particles[1, :x] == centers[5].x
+    @test particles[1, :y] == centers[5].y
 
     # Check warnings
     wrapped = AxisArray(img, Axis{:y}(0.5μm:0.5μm:50μm), Axis{:x}(1μm:1μm:100μm), Axis{:time}(1:5));
-    @test_logs (:warn, "Different scaling for x and y axes is not supported") SegmentationTools.build_tp_df(wrapped, thresholds)
+    @test_logs (:warn, "Different scaling for x and y axes is not supported") SegmentationTools.build_tp_df(wrapped, mask)
 end
 
 @testset "local background calc" begin
